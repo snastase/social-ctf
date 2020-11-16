97c1025132f9aabd0dce33d4596a4d07569338ad
Sam Nastase <sam.nastase@gmail.com>
2020-11-15 21:58:00 -0500

    Compute ISC for all within-population matchups

--- isc.py
+++ isc.py
@@ -32,50 +32,70 @@ repeat_id = 0 # 0-7
 player_id = 0 # 0-3
 
 n_lstms = 512
+n_repeats = 8
+n_players = 4
+n_pairs = n_players * (n_players - 1) // 2
 
 # Print out some structure of the HDF5 dataset for convenience
 combined_key = ''
 for key in ['map', 'matchup', 'repeat', 'player', 'time']:
     combined_key += key + '/'
     print(f"{combined_key}: \n\t{list(wrap_f[combined_key].keys())}\n")
+    
+    
+# Get matchups with all same agents (e.g. AA vs AA)
+agent_ids = wrap_f['map/matchup/repeat/player/agent_id'][0, :, :, :, 0]
+matchup_ids = np.all(agent_ids[:, 0, :] == 
+                     agent_ids[:, 0, 0][:, np.newaxis], axis=1)
+n_matchups = np.sum(matchup_ids)
 
 
 # Extract LSTMs for one map and matchup
-lstms_rep = wrap_f['map/matchup/repeat/player/time/lstm'][
-    map_id, matchup_id, ...].astype(np.float32)
+lstms_matched = wrap_f['map/matchup/repeat/player/time/lstm'][
+    map_id, matchup_ids, ...].astype(np.float32)
+print("Loaded post-LSTMs for within-population matchups")
 
-# Loop through repeats
-for lstms in lstms_rep:
-    lstms = np.rollaxis(np.tanh(lstms), 0, 3)
+# Loop through matchups and repeats
+isc_results = np.zeros((n_matchups, n_repeats, n_pairs, n_lstms))
+for matchup in np.arange(n_matchups):
+    for repeat in np.arange(n_repeats):
+    
+        lstms = lstms_matched[matchup, repeat, ...]
+        lstms = np.rollaxis(lstms, 0, 3)
 
-    # Compute ISCs between each pair for 4 agents
-    iscs = isc(lstms, pairwise=True)
+        # Compute ISCs between each pair for 4 agents
+        iscs = isc(lstms, pairwise=True)
+        isc_results[matchup, repeat, ...] = iscs
 
-    # Squareform ISCs for matrix visualization
-    iscs_sq = []
-    for u in iscs.T:
-        u = squareform(u, checks=False)
-        np.fill_diagonal(u, 1)
-        iscs_sq.append(u)
-    iscs_sq = np.dstack(iscs_sq)
+        # Squareform ISCs for matrix visualization
+        iscs_sq = []
+        for u in iscs.T:
+            u = squareform(u, checks=False)
+            np.fill_diagonal(u, 1)
+            iscs_sq.append(u)
+        iscs_sq = np.dstack(iscs_sq)
 
-    # Cooperative and competitive pairs from 4 x 4 (6) pairs
-    coop, comp = [0, 5], [1, 2, 3, 4]
+        # Cooperative and competitive pairs from 4 x 4 (6) pairs
+        coop, comp = [0, 5], [1, 2, 3, 4]
 
-    iscs_coop = iscs[coop, :]
-    iscs_comp = iscs[comp, :]
+        iscs_coop = iscs[coop, :]
+        iscs_comp = iscs[comp, :]
 
-    plt.plot(fisher_mean(iscs_coop, axis=0))
-    plt.plot(fisher_mean(iscs_comp, axis=0))
-    plt.show()
+        plt.plot(fisher_mean(iscs_coop, axis=0))
+        plt.plot(fisher_mean(iscs_comp, axis=0))
+        plt.show()
 
-    iscs_diff = np.tanh(np.arctanh(fisher_mean(iscs_coop, axis=0)) -
-                        np.arctanh(fisher_mean(iscs_comp, axis=0)))
-    plt.plot(iscs_diff)
-    plt.show()
-    print(f"Mean cooperative ISC across units: {fisher_mean(iscs_coop):.3f}\n"
-          f"Mean competitive ISC across units: {fisher_mean(iscs_comp):.3f}\n"
-          "Difference between coperative vs competitive ISC: "
-          f"{fisher_mean(iscs_coop) - fisher_mean(iscs_comp):.3f}")
-    print("Proportion of units with cooperative > competitive ISC: "
-          f"{np.sum(iscs_diff > 0) / n_lstms:.3f}")
+        iscs_diff = np.tanh(np.arctanh(fisher_mean(iscs_coop, axis=0)) -
+                            np.arctanh(fisher_mean(iscs_comp, axis=0)))
+        plt.plot(iscs_diff)
+        plt.show()
+        print(f"Mean cooperative ISC (matchup {matchup}, repeat {repeat}): "
+              f"{fisher_mean(iscs_coop):.3f}\n"
+              f"Mean competitive ISC (matchup {matchup}, repeat {repeat}): "
+              f"{fisher_mean(iscs_comp):.3f}\n"
+              "Difference between coperative vs competitive ISC: "
+              f"{fisher_mean(iscs_coop) - fisher_mean(iscs_comp):.3f}")
+        print("Proportion of units with cooperative > competitive ISC: "
+              f"{np.sum(iscs_diff > 0) / n_lstms:.3f}")
+
+np.save('results/isc_lstm_results.npy', isc_results)
