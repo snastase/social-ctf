9b31a72c6771e1639dfe38b9356f35806f7d01ae
Sam Nastase <sam.nastase@gmail.com>
2021-01-09 17:33:40 -0500

    First pass on spatial ISC

--- isc.py
+++ isc.py
@@ -2,7 +2,7 @@ from os.path import join
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.spatial.distance import squareform
-from brainiak.isc import isc
+from brainiak.isc import isc, isfc
 
 # Load helper function(s) for interacting with CTF dataset
 from ctf_dataset.load import create_wrapped_dataset
@@ -45,12 +45,13 @@ n_matchups = np.sum(matchup_ids) # 0, 34, 49, 54
 
 
 # Extract LSTMs for one map and matchup
-lstms_matched = wrap_f['map/matchup/repeat/player/time/post_lstm'][
+lstms_matched = wrap_f['map/matchup/repeat/player/time/lstm'][
     map_id, matchup_ids, ...].astype(np.float32)
-print("Loaded post-LSTMs for within-population matchups")
+print("Loaded LSTMs for within-population matchups")
 
 # Loop through matchups and repeats
-isc_results = np.zeros((n_matchups, n_repeats, n_pairs, n_lstms))
+isc_results = np.zeros((n_matchups, n_repeats, n_pairs,
+                        lstms_matched.shape[-1]))
 for matchup in np.arange(n_matchups):
     for repeat in np.arange(n_repeats):
     
@@ -124,8 +125,8 @@ def plot_lstm_grid(lstms, n_rows=16, n_cols=32, title=None, **kwargs):
 plot_lstm_grid(iscs_comp[3], title='competition ISC (matchup 54)',
                vmin=0, vmax=1)
     
-plot_lstm_grid(isc_diffs[2],
-               title='cooperation – competition ISC (matchup 34)',
+plot_lstm_grid(isc_diffs[3],
+               title='cooperation – competition ISC (matchup 54)',
                cmap='RdBu_r', vmin=-1, vmax=1)
 
 
@@ -165,3 +166,101 @@ plot_lstm_grid(np.where(diff_ps < .05, diff_corrs, np.nan)[m],
                       'difference in score across repeats '
                       '(p < .05; matchup 49)'),
                cmap='RdBu_r', vmin=-1, vmax=1)
+
+
+# Compute spatial ISC (ISPC) per time point
+n_samples = 4501
+
+# Extract LSTMs for one map and matchup
+lstms_matched = wrap_f['map/matchup/repeat/player/time/post_lstm'][
+    map_id, matchup_ids, ...].astype(np.float32)
+print("Loaded post-LSTMs for within-population matchups")
+
+# Loop through matchups and repeats
+ispc_results = np.zeros((n_matchups, n_repeats, n_pairs, n_samples, n_samples))
+for matchup in np.arange(n_matchups):
+    for repeat in np.arange(n_repeats):
+    
+        lstms = lstms_matched[matchup, repeat, ...]
+        lstms = np.rollaxis(lstms, 0, 3)
+        lstms = np.rollaxis(lstms, 0, 2)
+
+        # Compute ISCs between each pair for 4 agents
+        ispcs = isfc(lstms, pairwise=True, vectorize_isfcs=False)
+        ispc_results[matchup, repeat, ...] = ispcs
+
+np.save('results/ispc_post-lstm_results.npy', isc_results)
+
+# Compare full results array
+coop_ids, comp_ids = [0, 5], [1, 2, 3, 4]
+
+ispcs_coop = np.mean(ispc_results[:, :, coop_ids, ...], axis=2)
+ispcs_comp = np.mean(ispc_results[:, :, comp_ids, ...], axis=2)
+
+# Specify an example repeat
+rep = 3
+pop_labels = ['AA vs. AA', 'BB vs. BB', 'CC vs. CC', 'DD vs. DD']
+fig, axs = plt.subplots(2, 2, figsize=(8, 8))
+plt.suptitle(f'cooperation – competition spatial ISC (example repeat {rep})',
+             y=.96)
+for ax, m, p in zip(axs.ravel(), np.arange(n_matchups), pop_labels):
+    mat = ax.matshow(ispcs_coop[m, rep] - ispcs_comp[m, rep], cmap='RdYlBu_r',
+                     vmin=-.2, vmax=.2)
+    ax.xaxis.set_tick_params(which='both', length=0)
+    ax.yaxis.set_tick_params(which='both', length=0)
+    ax.set_title(p)
+    if m > 1:
+        ax.set_xlabel('time points')
+fig.colorbar(mat, ax=axs.ravel().tolist(), fraction=0.025, pad=0.04);
+
+# Get the average ISPC across episodes
+pop_labels = ['AA vs. AA', 'BB vs. BB', 'CC vs. CC', 'DD vs. DD']
+fig, axs = plt.subplots(2, 2, figsize=(8, 8))
+plt.suptitle(f'cooperation – competition spatial ISC (example repeat {e})',
+             y=.96)
+for ax, m, p in zip(axs.ravel(), np.arange(n_matchups), pop_labels):
+    mat = ax.matshow((np.mean(ispcs_coop[m], axis=0) -
+                      np.mean(ispcs_comp[m], axis=0)),
+                     cmap='RdYlBu_r', vmin=-.13, vmax=.13)
+    ax.xaxis.set_tick_params(which='both', length=0)
+    ax.yaxis.set_tick_params(which='both', length=0)
+    ax.set_title(p)
+    if m > 1:
+        ax.set_xlabel('time points')
+fig.colorbar(mat, ax=axs.ravel().tolist(), fraction=0.025, pad=0.04)
+
+
+# Get diagonal ISPC values i.e. time-point-by-time-point
+ispcs_diag = np.diagonal(ispc_results, axis1=-2, axis2=-1)[..., coop_ids, :]
+captures = wrap_f['map/matchup/repeat/player/time/events'][0, matchup_ids, ..., 7]
+
+matchup = 0
+repeat = 0
+#plt.plot(captures[matchup, repeat, 0], color='maroon')
+#plt.plot(captures[matchup, repeat, 2], color='darkblue')
+#plt.plot(np.zeros(4501), color='gray')
+plt.plot(ispcs_diag[matchup, repeat, 0], color='tomato', alpha=.5)
+plt.plot(ispcs_diag[matchup, repeat, 1], color='deepskyblue', alpha=.5)
+#plt.plot(ispcs_diag[matchup, repeat, 1], color='deepskyblue', alpha=.5)
+plt.plot((np.cumsum(captures[matchup, repeat, 0]) /
+          np.amax([np.amax(np.cumsum(captures[matchup, repeat, 0])),
+                   np.amax(np.cumsum(captures[matchup, repeat, 2]))])), color='maroon')
+plt.plot((np.cumsum(captures[matchup, repeat, 2]) /
+          np.amax([np.amax(np.cumsum(captures[matchup, repeat, 0])),
+                   np.amax(np.cumsum(captures[matchup, repeat, 2]))])), color='darkblue')
+print(f'{pearsonr(captures[matchup, repeat, 0], ispcs_diag[matchup, repeat, 0])[0]:.3f}')
+print(f'{pearsonr(captures[matchup, repeat, 2], ispcs_diag[matchup, repeat, 1])[0]:.3f}')
+#plt.plot(captures[matchup, repeat, 2], color='darkblue')
+plt.plot(np.zeros(4501), color='gray')
+
+
+
+
+
+
+scores = wrap_f['map/matchup/repeat/player/my_team_score'][
+            0, :, :, [0, 2], :][matchup_ids][..., 0]
+coop_ids
+reward = wrap_f['map/matchup/repeat/player/time/reward'][0, matchup_ids, :, :, :, 0]
+values = wrap_f['map/matchup/repeat/player/time/reward'][0, matchup_ids, :, :, :, 0]
+
