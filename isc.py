e3d7064b89b54c822f536c8c0a6b01fdc671d9cf
Sade Abiodun <fabiodun@princeton.edu>
2021-03-26 14:54:43 -0400

    correlated ISPC with game variables

--- isc.py
+++ isc.py
@@ -1,8 +1,12 @@
 from os.path import join
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.stats import pearsonr
 from scipy.spatial.distance import squareform
+from itertools import combinations
 from brainiak.isc import isc, isfc
+from features import get_features
+from features import get_events
 
 # Load helper function(s) for interacting with CTF dataset
 from ctf_dataset.load import create_wrapped_dataset
@@ -18,6 +22,7 @@ def fisher_mean(correlations, axis=None):
 
 
 # Create wrapped CTF dataset
+# This picks out the matchups (self vs. self) in which the behavior is easiest to identify
 wrap_f = create_wrapped_dataset(data_dir, output_dataset_name="virtual.hdf5")
 
 map_id = 0 # 0
@@ -31,6 +36,9 @@ n_players = 4
 n_pairs = n_players * (n_players - 1) // 2
 
 # Print out some structure of the HDF5 dataset for convenience
+
+# to look up what you're pulling from within the hdf5, index through to the desired level and then add .keys 
+#something like wrap_f['map/matchup/repeat/player/time'].keys() or agent_ids.shape will reveal more about the internal structure
 combined_key = ''
 for key in ['map', 'matchup', 'repeat', 'player', 'time']:
     combined_key += key + '/'
@@ -45,11 +53,15 @@ n_matchups = np.sum(matchup_ids) # 0, 34, 49, 54
 
 
 # Extract LSTMs for one map and matchup
-lstms_matched = wrap_f['map/matchup/repeat/player/time/lstm'][
-    map_id, matchup_ids, ...].astype(np.float32)
+# this is where we make the active choice to use post_lstm vs lstm
+# we're tanh'ing the data as it's loaded in to truncate the values to a measurable range 
+lstms_matched = np.tanh(wrap_f['map/matchup/repeat/player/time/lstm'][
+    map_id, matchup_ids, ...].astype(np.float32)) 
 print("Loaded LSTMs for within-population matchups")
 
+
 # Loop through matchups and repeats
+# The next 10 lines are the ones that are actually GENERATING the data (up until squareform)
 isc_results = np.zeros((n_matchups, n_repeats, n_pairs,
                         lstms_matched.shape[-1]))
 for matchup in np.arange(n_matchups):
@@ -93,12 +105,14 @@ for matchup in np.arange(n_matchups):
         print("Proportion of units with cooperative > competitive ISC: "
               f"{np.sum(iscs_diff > 0) / n_lstms:.3f}")
 
-np.save('results/isc_lstm_results.npy', isc_results)
+np.save('results/isc_post-lstm_results.npy', isc_results)
+# commenting out the above line for now because it's breaking le code 
 
 
 # Compare full results array
 coop_ids, comp_ids = [0, 5], [1, 2, 3, 4]
 
+# isc results index is all repeats, all matchups, coop/comp pairs, each LSTM
 iscs_coop = np.mean(np.mean(isc_results[:, :, coop_ids, :], axis=2), axis=1)
 iscs_comp = np.mean(np.mean(isc_results[:, :, comp_ids, :], axis=2), axis=1)
 
@@ -168,13 +182,21 @@ plot_lstm_grid(np.where(diff_ps < .05, diff_corrs, np.nan)[m],
                cmap='RdBu_r', vmin=-1, vmax=1)
 
 
+# IMPORTANT SECTION
 # Compute spatial ISC (ISPC) per time point
 n_samples = 4501
+map_id = 0
+matchup_ids = [0, 34, 49, 54]
+n_matchups = 4
+n_repeats = 8
+n_pairs = 6
 
-# Extract LSTMs for one map and matchup
-lstms_matched = wrap_f['map/matchup/repeat/player/time/post_lstm'][
-    map_id, matchup_ids, ...].astype(np.float32)
-print("Loaded post-LSTMs for within-population matchups")
+# Extract LSTMs for one map and matchup (switch out post_lstm and lstm depending on analysis)
+# we're tanh'ing the data as it's loaded in to truncate the values to a measurable range
+lstms_matched = np.tanh(wrap_f['map/matchup/repeat/player/time/lstm'][
+    map_id, matchup_ids, ...].astype(np.float32)) # swtich with post lstm when needed
+print("Loaded LSTMs for within-population matchups")
+
 
 # Loop through matchups and repeats
 ispc_results = np.zeros((n_matchups, n_repeats, n_pairs, n_samples, n_samples))
@@ -188,8 +210,10 @@ for matchup in np.arange(n_matchups):
         # Compute ISCs between each pair for 4 agents
         ispcs = isfc(lstms, pairwise=True, vectorize_isfcs=False)
         ispc_results[matchup, repeat, ...] = ispcs
+        print(f"finished ispc for matchup {matchup} repeat {repeat}")
 
-np.save('results/ispc_post-lstm_results.npy', isc_results)
+#np.save('results/ispc_post-lstm_results.npy', ispc_results) 
+np.save('results/ispc_lstm_tanh_results.npy', ispc_results)
 
 # Compare full results array
 coop_ids, comp_ids = [0, 5], [1, 2, 3, 4]
@@ -216,8 +240,9 @@ fig.colorbar(mat, ax=axs.ravel().tolist(), fraction=0.025, pad=0.04);
 # Get the average ISPC across episodes
 pop_labels = ['AA vs. AA', 'BB vs. BB', 'CC vs. CC', 'DD vs. DD']
 fig, axs = plt.subplots(2, 2, figsize=(8, 8))
-plt.suptitle(f'cooperation – competition spatial ISC (example repeat {e})',
+plt.suptitle(f'cooperation – competition spatial ISC (example repeat {rep})',
              y=.96)
+# SADE: replaced e w/ rep to see if that clears the error 
 for ax, m, p in zip(axs.ravel(), np.arange(n_matchups), pop_labels):
     mat = ax.matshow((np.mean(ispcs_coop[m], axis=0) -
                       np.mean(ispcs_comp[m], axis=0)),
@@ -254,13 +279,180 @@ print(f'{pearsonr(captures[matchup, repeat, 2], ispcs_diag[matchup, repeat, 1])[
 plt.plot(np.zeros(4501), color='gray')
 
 
-
-
-
-
 scores = wrap_f['map/matchup/repeat/player/my_team_score'][
             0, :, :, [0, 2], :][matchup_ids][..., 0]
 coop_ids
 reward = wrap_f['map/matchup/repeat/player/time/reward'][0, matchup_ids, :, :, :, 0]
 values = wrap_f['map/matchup/repeat/player/time/reward'][0, matchup_ids, :, :, :, 0]
+
+
+### 
+
+# SADE: Test running one match/one repeat ISC and correlating against game variable 
+
+# Load in existing ISPC results (in place of code chunk above)
+ispc_results = np.load('results/ispc_lstm_tanh_results.npy')
+
+
+# Exclude degenerate features from analysis 
+feature_set = ['position', 'health', 'events']
+all_features, labels = get_features(wrap_f, feature_set=feature_set, map_id=map_id,
+                                    matchup_id=matchup_ids, player_id=slice(None),
+                                    repeat_id=slice(None))
+
+features_exclude = []
+for label in labels: 
+    features = all_features[..., np.array(labels) == label]
+    n_nonzeros = np.sum(np.nonzero(features))
+    print(f'checking {label} for all nonzeros; found {n_nonzeros} nonzeros')
+    if n_nonzeros == 0:
+        features_exclude.append(label)
+        print(f'excluding {label}')
+        
+# Get proximities
+def get_proximity(position):
+    
+    # Ignore z-position for now
+    position = position[..., :2]
+
+    # Compute Euclidean distance over time for all pay ers of players
+    proximity = np.full(tuple(np.array(position.shape)[[0, 1, 3]]) + (6,), np.nan)
+    for matchup in np.arange(position.shape[0]):
+        for repeat in np.arange(position.shape[1]):
+            for p, pair in enumerate(combinations(np.arange(position.shape[2]), 2)):
+                proximity[matchup, repeat, :, p] = np.sqrt(np.sum((position[matchup, repeat, pair[0], ...] -
+                                                 position[matchup, repeat, pair[1], ...]) ** 2,
+                                                            axis=1))
+    return proximity
+
+position, _ = get_features(wrap_f, feature_set=['position'], map_id=map_id,
+                           matchup_id=matchup_ids, player_id=slice(None),
+                           repeat_id=slice(None))
+
+proximity = get_proximity(position)
+
+# Compare full results array
+coop_ids, comp_ids = [0, 5], [1, 2, 3, 4]
+
+# Compute correlation between game variable and ISPC 
+
+#variables = wrap_f['map/matchup/repeat/player/time'].keys()
+
+# Initialize dict to sort between pairs and individual players 
+pair_player = {pair: player for pair, player in zip(np.arange(6), combinations(np.arange(4), 2))}
+
+ispc_corrs = {} # big ultimate result dict 
+
+#game_stats =  wrap_f['map/matchup/repeat/player/time'][game_var] # pull variable info
+#matchup_stat = game_stats[map_id, matchup_id, rep_id, ...]
+map_id = 0 
+matchup_ids = [0, 34, 49, 54]
+n_repeats = 8
+
+
+# define a single variable to pull the stats for (this may be redundant)
+
+for game_var in labels:
+    # skips degenerate game variables 
+    if game_var in features_exclude: 
+        continue 
+    features = all_features[..., np.array(labels) == game_var]
+    #ispc_feature_corr[game_var] = {} # internal dict for game_var
+    feature_shape = features.shape[:-2]
+    if len(feature_shape) == 3: 
+        ispc_corrs[game_var] = np.full(feature_shape[:-1] + (6, 2), np.nan)
+    elif len(feature_shape) == 2:
+        ispc_corrs[game_var] = np.full(feature_shape + (6,), np.nan)
+
+    #ispc_corrs[game_var] = np.full((len(matchup_ids),n_repeats,6,2), np.nan)
+    #n_nans = 0
+    for m, matchup_id in enumerate(matchup_ids):
+        for repeat_id in np.arange(n_repeats):   
+            # loop through and extract player ids from each pairing
+            for pair_id in np.arange(6): 
+                #isolate the ispc for each player within the pair
+                #if np.sum(features[m, repeat_id, pair_player[pair_id][0], :, 0] == 0) == n_samples: 
+                #    n_nans += 1
+                #    print(f"WARNING: {game_var} is all zeros for matchup {matchup_id} and repeat {repeat_id}")
+                ispcs = np.diagonal(ispc_results[map_id, m, pair_id, ...])
+                #print("ispc_coop for", pair_id, "is", ispc_coop.shape)
+                if len(feature_shape) == 3: 
+                    pl1_corr = pearsonr(features[m, repeat_id, pair_player[pair_id][0], :, 0], ispcs)[0]
+                    pl2_corr = pearsonr(features[m, repeat_id, pair_player[pair_id][1], :, 0], ispcs)[0]                    
+                    ispc_corrs[game_var][m, repeat_id, pair_id, 0] = pl1_corr
+                    ispc_corrs[game_var][m, repeat_id, pair_id, 1] = pl2_corr
+                elif len(feature_shape) == 2:
+                    team_corr = pearsonr(features[m, repeat_id, :, 0], ispcs)[0]                    
+                    ispc_corrs[game_var][m, repeat_id, pair_id] = team_corr
+
+    #assert np.sum(np.isnan(ispc_corrs[game_var])) > n_nans # check to make sure array is filled w/ real values 
+    print(f"finished ispc correlations w/ {game_var}")
+
+# Save dictionary 
+np.save('results/lstm_ispc_tanh_feature_correlations.npy', ispc_corrs) #switch with post when appropriate 
+
+# To load dictionary
+ispc_corrs = np.load('results/lstm_ispc_tanh_feature_correlations.npy', allow_pickle=True).item() #switch with post when appropriate 
+
+# Compare ispc across repeats and matchups
+coop_ids, comp_ids = [0, 5], [1, 2, 3, 4]
+
+
+# Coop Plot 
+
+# Summarize ISPC Corrs across pairs and repeats
+ispc_corr_coop_means = []
+
+for game_var in ispc_corrs:
+    if ispc_corrs[game_var].ndim == 4:
+        ispc_corr_coop = ispc_corrs[game_var][..., coop_ids, :]
+        ispc_corr_coop_means.append(np.nanmean(ispc_corr_coop, axis=(1, 2, 3)))
+    elif ispc_corrs[game_var].ndim == 3:
+        ispc_corr_coop = ispc_corrs[game_var][..., coop_ids]
+        ispc_corr_coop_means.append(np.nanmean(ispc_corr_coop, axis=(1, 2)))
+        
+ispc_corr_coop_means = np.column_stack(ispc_corr_coop_means)
+
+
+# Summarize ISPC Corrs across pairs and repeats
+ispc_corr_comp_means = []
+
+for game_var in ispc_corrs:
+    if ispc_corrs[game_var].ndim == 4:
+        ispc_corr_comp = ispc_corrs[game_var][..., comp_ids, :]
+        ispc_corr_comp_means.append(np.nanmean(ispc_corr_comp, axis=(1, 2, 3)))
+    elif ispc_corrs[game_var].ndim == 3:
+        ispc_corr_comp = ispc_corrs[game_var][..., comp_ids]
+        ispc_corr_comp_means.append(np.nanmean(ispc_corr_comp, axis=(1, 2)))
+
+#eventually try to figure out how to get proximities and game status (winning vs. losing) 
+ispc_corr_comp_means = np.column_stack(ispc_corr_comp_means)
+
+
+# sorts between populations blah blah blah 
+sorter = np.argsort(np.nanmean(np.vstack([ispc_corr_coop_means, 
+                                          ispc_corr_comp_means]), axis=0))[::-1]
+
+ispc_corr_coop_sorted = ispc_corr_coop_means[:, sorter]
+ispc_corr_comp_sorted = ispc_corr_comp_means[:, sorter]
+xlabels = np.array(list(ispc_corrs.keys()))[sorter]
+
+#store variables to be manually exluded 
+win_vars = ['player draw player', 'player loss player', 'player win player']
+
+# Attempt to plot!!
+
+#Coop 
+
+plt.matshow(ispc_corr_coop_means[:, sorter], vmin=-.05, vmax=.05, cmap='RdBu_r')
+plt.yticks([0, 1, 2, 3], ['A','B','C','D'])
+plt.xticks(np.arange(ispc_corr_coop_means.shape[1]), xlabels, rotation=90);
+plt.title("Cooperative")
+
+#Comp 
+
+plt.matshow(ispc_corr_comp_means[:, sorter], vmin=-.05, vmax=.05, cmap='RdBu_r')
+plt.yticks([0, 1, 2, 3], ['A','B','C','D'])
+plt.xticks(np.arange(ispc_corr_comp_means.shape[1]), xlabels, rotation=90);
+plt.title("Competitive")
 
